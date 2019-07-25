0d3f12c26837c43b4d45e87af15e91971b62817e
Igor Andruskiewitsch <igor.andruskiewitsch@incluit.com>
2019-07-25 17:03:11 -0300

    Initial Commit

---
+++ MemeGenerator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MemeGenerator
+//
+//  Created by Igor Andruskiewitsch on 7/25/19.
+//  Copyright © 2019 Igor Andruskiewitsch. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
